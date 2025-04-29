6b625652083436146f32cbc96c59685a77877dfb
Mann Kandoi <mannkandoi2005@gmail.com>
2025-04-29 11:44:33 +0530

    Add files via upload

---
+++ JavaScript/Variables.js
@@ -0,0 +1,10 @@
+let name = "Mann";   //Let is used to Declare a Variable
+const age = 20;     //Const is for Constants(values that don't change)
+var city = "Vadodara";   // var is  an older way to declare a varaiable
+
+// let is used when you want to reassign the value of a variable later.
+
+// const is used when the value should not change after it's assigned.
+
+// var is an older keyword, but it still works. It's best to use let or const in modern code.
+
---
+++ JavaScript/WhileLoop.js
@@ -0,0 +1,6 @@
+// Write a program to print numbers from 1 to 5 using a while loop.
+let i = 1;
+while(i <= 5){
+    console.log(i);
+    i++;
+}
